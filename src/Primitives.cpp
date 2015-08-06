77394bb1bec749530cc8872f8517638299ea31c3
Andrei Alexandru <alexandruandrei91@gmail.com>
2015-08-06 10:51:55 +0300

    Add plane primitive

--- src/Primitives.cpp
+++ src/Primitives.cpp
@@ -1,4 +1,4 @@
-#include "BoxRenderObject.h"
+#include "Primitives.h"
 #define UNIT 1.0f
 
 TakeOne::BoxRenderObject::BoxRenderObject(const std::shared_ptr<Program>& pProgram) :
@@ -61,6 +61,38 @@ TakeOne::BoxRenderObject::BoxRenderObject(const std::shared_ptr<Program>& pProgr
 
         20, 21, 22,
         20, 22, 23
+    };
+
+    mMesh->SetIndices(std::move(indices));
+
+    mMesh->Setup();
+}
+
+TakeOne::PlaneRenderObject::PlaneRenderObject(const std::shared_ptr<Program>& pProgram) :
+    RenderObject(pProgram)
+{
+    std::vector<unsigned int> attribsUsed(Vertex::Count);
+    attribsUsed[Vertex::POSITION] = 1;
+    attribsUsed[Vertex::NORMAL] = 1;
+    attribsUsed[Vertex::COLOR] = 0;
+    attribsUsed[Vertex::TEXCOORD] = 1;
+
+    mMesh->SetAttribsUsed(std::move(attribsUsed));
+
+    std::vector<Vertex> vertices
+    {
+        Vertex(glm::vec3( UNIT, -UNIT,  UNIT), glm::vec3(-0.0, -0.0,  1.0), glm::vec3(0.0), glm::vec2( 0.0,  0.0)),
+        Vertex(glm::vec3( UNIT,  UNIT,  UNIT), glm::vec3(-0.0, -0.0,  1.0), glm::vec3(0.0), glm::vec2( 1.0,  0.0)),
+        Vertex(glm::vec3(-UNIT,  UNIT,  UNIT), glm::vec3(-0.0, -0.0,  1.0), glm::vec3(0.0), glm::vec2( 1.0,  1.0)),
+        Vertex(glm::vec3(-UNIT, -UNIT,  UNIT), glm::vec3(-0.0, -0.0,  1.0), glm::vec3(0.0), glm::vec2( 0.0,  1.0)),
+    };
+
+    mMesh->SetVertices(std::move(vertices));
+
+    std::vector<unsigned int> indices
+    {
+        0, 1, 2,
+        0, 2, 3,
     };
 
     mMesh->SetIndices(std::move(indices));
